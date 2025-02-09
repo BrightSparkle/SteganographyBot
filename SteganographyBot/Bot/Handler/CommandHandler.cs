9ec35150ee5f8fea18ea5ac3c73af773d17d5322
BrightSparkle <ekaterinamrch@gmail.com>
2025-02-09 17:45:35 +0300

    Add files

--- SteganographyBot/Bot/Handler/CommandHandler.cs
+++ SteganographyBot/Bot/Handler/CommandHandler.cs
@@ -8,7 +8,7 @@ namespace SteganographyBot;
 
 public class CommandHandler
 {
-    private readonly Dictionary<long, ChatState> _chatStates ;
+    private readonly Dictionary<long, ChatState> _chatStates;
     private readonly Encryptor _encryptor;
     private readonly Decryptor _decryptor;
 
@@ -19,211 +19,193 @@ public class CommandHandler
         _decryptor = new Decryptor();
     }
 
+    // Entry point to handle incoming messages
     public async void HandleCommand(ITelegramBotClient telegramClient, Update update)
     {
-        long chatId = update.Message.Chat.Id;
+        var chatId = update.Message.Chat.Id;
 
         // Initialize chat state if it doesn't exist
-        if (!_chatStates.ContainsKey(chatId))
-        {
-            _chatStates[chatId] = ChatState.Base;
-        }
+        InitializeChatState(chatId);
 
-        
-        
         switch (update.Message.Type)
         {
-            
-            
-            
             case MessageType.Text:
-            {
-                
-                
-                
-                
-                if (_chatStates[chatId] != ChatState.Base)
-                {
-                    if (_chatStates[chatId] == ChatState.AwaitingImageAndText)
-                    {
-                        await telegramClient.SendMessage(update.Message.Chat.Id,
-                            "Needed image and text for encoding. Going back.");
-                        _chatStates[chatId] = ChatState.Base;
-                        break;
-                    }
-
-                    if (_chatStates[chatId] == ChatState.AwaitingDecodeImage)
-                    {
-                        await telegramClient.SendMessage(update.Message.Chat.Id,
-                            "Needed image for decoding. Going back.");
-                        _chatStates[chatId] = ChatState.Base;
-                        break;
-                    }
-                }
-                
-                
-                
-                
-                
-                
-                switch (update.Message.Text)
-                {
-                    case "/help":
-                    {   //TODO нафигачь описания
-                        await telegramClient.SendMessage(update.Message.Chat.Id,
-                            "This bot can encode and decode messages in images.");
-                        break;
-                    }
-
-                    case "/encode":
-                    {
-                        // Change state to expect an image with a caption for encoding
-                        await telegramClient.SendMessage(update.Message.Chat.Id,
-                            "Please send an image with a caption for encoding.");
-                        _chatStates[chatId] = ChatState.AwaitingImageAndText;
-                        break;
-                    }
-
-                    case "/decode":
-                    {
-                        // Change state to expect only an image for decoding
-                        await telegramClient.SendMessage(update.Message.Chat.Id,
-                            "Please send an image for decoding.");
-                        _chatStates[chatId] = ChatState.AwaitingDecodeImage;
-                        break;
-                    }
-
-                    default:
-                    {
-                        await telegramClient.SendMessage(update.Message.Chat.Id, "Unknown command. Please use /help.");
-                        break;
-                    }
-                        
-                }
-
+                await HandleTextMessage(telegramClient, update, chatId);
                 break;
-            }
-            
-            
-            
-            
-            
 
             case MessageType.Photo:
-            {
-                    
-                await HandlePhotoAsync(telegramClient, update);
+                await HandlePhotoAsync(telegramClient, update, chatId);
+                break;
 
-                
-                break;
-            }
             default:
-            {
                 await telegramClient.SendMessage(update.Message.Chat.Id, "Wrong message type. Please use /help.");
+                _chatStates[chatId] = ChatState.Base;
                 break;
-            }
-            
-            //рофло-комментарий
-            
         }
-        
     }
 
-    
-    
-    
-    
-    
-    public async Task HandlePhotoAsync(ITelegramBotClient botClient, Update update)
+    // Initialize chat state
+    private void InitializeChatState(long chatId)
     {
-        
-        long chatId = update.Message.Chat.Id;
+        if (!_chatStates.ContainsKey(chatId)) _chatStates[chatId] = ChatState.Base;
+    }
 
-        
-        // Check if the user is in the correct state to receive an image
+
+    // Handle text messages (commands)
+    private async Task HandleTextMessage(ITelegramBotClient telegramClient, Update update, long chatId)
+    {
+        if (_chatStates[chatId] != ChatState.Base)
+        {
+            await HandleInvalidStateMessage(telegramClient, update, chatId);
+            return;
+        }
+
+        switch (update.Message.Text)
+        {
+            case "/start":
+                break;
+
+            case "/help":
+                await SendHelpMessage(telegramClient, update);
+                break;
+
+            case "/encode":
+                await HandleEncodeCommand(telegramClient, update, chatId);
+                break;
+
+            case "/decode":
+                await HandleDecodeCommand(telegramClient, update, chatId);
+                break;
+
+            default:
+                await telegramClient.SendMessage(update.Message.Chat.Id, "Unknown command. Please use /help.");
+                break;
+        }
+    }
+
+
+    // Send help message
+    private async Task SendHelpMessage(ITelegramBotClient telegramClient, Update update)
+    {
+        await telegramClient.SendMessage(update.Message.Chat.Id, "This bot can encode and decode messages in images.");
+    }
+
+
+    // Handle /encode command
+    private async Task HandleEncodeCommand(ITelegramBotClient telegramClient, Update update, long chatId)
+    {
+        await telegramClient.SendMessage(update.Message.Chat.Id, "Please send an image with a caption for encoding.");
+        _chatStates[chatId] = ChatState.AwaitingImageAndText;
+    }
+
+
+    // Handle /decode command
+    private async Task HandleDecodeCommand(ITelegramBotClient telegramClient, Update update, long chatId)
+    {
+        await telegramClient.SendMessage(update.Message.Chat.Id, "Please send an image for decoding.");
+        _chatStates[chatId] = ChatState.AwaitingDecodeImage;
+    }
+
+
+    // Handle invalid state (if a command is issued in an invalid state)
+    private async Task HandleInvalidStateMessage(ITelegramBotClient telegramClient, Update update, long chatId)
+    {
+        if (_chatStates[chatId] == ChatState.AwaitingImageAndText)
+            await telegramClient.SendMessage(update.Message.Chat.Id, "Needed image and text for encoding. Going back.");
+        else if (_chatStates[chatId] == ChatState.AwaitingDecodeImage)
+            await telegramClient.SendMessage(update.Message.Chat.Id, "Needed image for decoding. Going back.");
+        _chatStates[chatId] = ChatState.Base;
+    }
+
+    // Handle photo messages for encoding and decoding
+    private async Task HandlePhotoAsync(ITelegramBotClient botClient, Update update, long chatId)
+    {
         if (!_chatStates.ContainsKey(chatId) || _chatStates[chatId] == ChatState.Base)
         {
             await botClient.SendMessage(chatId, "Please use /encode or /decode or /help to start the process.");
             return;
         }
-        
 
-        if (_chatStates[chatId]== ChatState.AwaitingImageAndText)
+
+        if (_chatStates[chatId] == ChatState.AwaitingImageAndText)
+            await ProcessImageForEncoding(botClient, update, chatId);
+        else if (_chatStates[chatId] == ChatState.AwaitingDecodeImage)
+            await ProcessImageForDecoding(botClient, update, chatId);
+    }
+
+
+    // Process image for encoding
+    private async Task ProcessImageForEncoding(ITelegramBotClient botClient, Update update, long chatId)
+    {
+        var photo = update.Message.Photo[0];
+        var file = await botClient.GetFile(photo.FileId);
+
+        using (var fileStream = new MemoryStream())
         {
-            // Handling the image for encoding (same as before)
-            if (update.Message.Type == MessageType.Photo)
+            await botClient.DownloadFile(file.FilePath, fileStream);
+            var imageBytes = fileStream.ToArray();
+            var caption = update.Message.Caption;
+
+            if (!string.IsNullOrEmpty(caption))
             {
-                var photo = update.Message.Photo[0]; // Get the largest photo
-
-                // Download the file
-                var file = await botClient.GetFileAsync(photo.FileId);
-                using (var fileStream = new MemoryStream())
-                {
-                    await botClient.DownloadFile(file.FilePath, fileStream);
-                    byte[] imageBytes = fileStream.ToArray();
-
-                    // Now you can process the image and caption
-                    string text = update.Message.Caption; // The caption (encoded message or data)
-
-                    if (text != null && text.Length > 0)
-                    {
-                        //ТУТ ДЕЛАЕМ ЭНКОД
-                        byte[] encoded = _encryptor.EncryptStringMessage(imageBytes, Encoding.UTF8.GetBytes(text),false);
-                        // Send back the image and caption for now (encoding response)
-                        await botClient.SendPhotoAsync(
-                            chatId: chatId,
-                            photo: new InputFileStream(new MemoryStream(encoded), "encoded_image.png"),
-                            caption: $"Received image for encoding: {text}"
-                        );
-                    }
-                    else
-                    {
-                        botClient.SendMessage(chatId, "Please send an image with not empty caption for encoding.");
-                    }
-
-                    // Reset state to Idle after processing
-                    _chatStates[chatId] = ChatState.Base;
-                }
+                var encodedImage = _encryptor.EncryptStringMessage(imageBytes, Encoding.UTF8.GetBytes(caption), false);
+                await SendEncodedImage(botClient, chatId, encodedImage, caption);
             }
             else
             {
-                await botClient.SendMessage(chatId, "Please send an image with a caption for encoding.");
+                await botClient.SendMessage(chatId, "Please send an image with a non-empty caption for encoding.");
+            }
+
+            _chatStates[chatId] = ChatState.Base;
+        }
+    }
+
+
+    // Send encoded image
+    public async Task SendEncodedImage(ITelegramBotClient botClient, long chatId, byte[] encodedImage, string caption)
+    {
+        try
+        {
+            // Попытка отправить изображение
+            using (var stream = new MemoryStream(encodedImage))
+            {
+                var photo = new InputFileStream(stream, "encoded_image.jpg");
+                await botClient.SendDocument(chatId, photo, caption);
             }
         }
-        else if (_chatStates[chatId] == ChatState.AwaitingDecodeImage)
+        catch (Exception ex)
         {
-            // Handling the image for decoding
-            if (update.Message.Type == MessageType.Photo)
+            // Логирование ошибки Telegram Bot API
+            Console.WriteLine($"Ошибка Telegram API: {ex.Message}");
+            // Дополнительно можно отправить сообщение об ошибке администратору
+            await botClient.SendMessage(chatId, "Произошла ошибка при обработке изображения.");
+        }
+    }
+
+
+    // Process image for decoding
+    private async Task ProcessImageForDecoding(ITelegramBotClient botClient, Update update, long chatId)
+    {
+        var photo = update.Message.Photo?[0];
+        var file = await botClient.GetFile(photo?.FileId);
+
+        using (var fileStream = new MemoryStream())
+        {
+            await botClient.DownloadFile(file.FilePath, fileStream);
+            var imageBytes = fileStream.ToArray();
+
+            if (string.IsNullOrEmpty(update.Message.Caption))
             {
-                var photo = update.Message.Photo[0]; // Get the largest photo
-
-                // Download the file
-                var file = await botClient.GetFileAsync(photo.FileId);
-                using (var fileStream = new MemoryStream())
-                {
-                    await botClient.DownloadFile(file.FilePath, fileStream);
-                    byte[] imageBytes = fileStream.ToArray();
-                    string text = update.Message.Caption;
-                    // Now you can process the image (decoding logic here)
-                    // For demonstration, we'll just send the image back
-                    if (String.IsNullOrEmpty(text))
-                    {
-                        byte[] decoded = _decryptor.DecryptStringMessage(imageBytes, false);
-                        await botClient.SendPhotoAsync(
-                            chatId: chatId,
-                            photo: new InputFileStream(new MemoryStream(decoded), "decoded_image.png"),
-                            caption: "Image received for decoding."
-                        );
-                    }
-                    else
-                    {
-                        await botClient.SendMessage(chatId, "Please send an image without caption for decoding.");
-                    }
-
-                    // Reset state to Idle after processing
-                    _chatStates[chatId] = ChatState.Base;
-                }
+                var decodedText = Encoding.UTF8.GetString(_decryptor.DecryptStringMessage(imageBytes, false));
+                await botClient.SendMessage(chatId, decodedText);
             }
+            else
+            {
+                await botClient.SendMessage(chatId, "Please send an image without caption for decoding.");
+            }
+
+            _chatStates[chatId] = ChatState.Base;
         }
     }
 }
+
