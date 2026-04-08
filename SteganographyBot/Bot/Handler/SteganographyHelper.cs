751e07184fa1ce349cfee2a0dce8218cdbe88d4e
BrightSparkle <ekaterinamrch@gmail.com>
2026-04-09 02:43:59 +0300

    Optimize algorithm

--- SteganographyBot/Bot/Handler/SteganographyHelper.cs
+++ SteganographyBot/Bot/Handler/SteganographyHelper.cs
@@ -46,28 +46,6 @@ public static class SteganographyHelper
         EmbedBits(image, bits);
     }
 
-    public static byte[] ExtractBytes(Bitmap image)
-    {
-        string bits = ExtractAllBits(image);
-        if (bits.Length < 32) return Array.Empty<byte>();
-
-        byte[] lengthBytes = new byte[4];
-        for (int i = 0; i < 4; i++)
-            lengthBytes[i] = Convert.ToByte(bits.Substring(i * 8, 8), 2);
-        if (BitConverter.IsLittleEndian)
-            Array.Reverse(lengthBytes);
-        int dataLength = BitConverter.ToInt32(lengthBytes, 0);
-
-        if (bits.Length < 32 + dataLength * 8)
-            return Array.Empty<byte>();
-
-        string dataBits = bits.Substring(32, dataLength * 8);
-        byte[] result = new byte[dataLength];
-        for (int i = 0; i < dataLength; i++)
-            result[i] = Convert.ToByte(dataBits.Substring(i * 8, 8), 2);
-        return result;
-    }
-
     private static void EmbedBits(Bitmap image, string bits)
     {
         int bitIndex = 0;
@@ -77,7 +55,6 @@ public static class SteganographyHelper
             {
                 Color pixel = image.GetPixel(x, y);
                 var (rBits, gBits, bBits) = ToBinary(pixel);
-
                 if (bitIndex + 2 <= bits.Length)
                 {
                     rBits = ReplaceFirstBits(rBits, bits.Substring(bitIndex, 2));
@@ -103,20 +80,70 @@ public static class SteganographyHelper
             throw new Exception("Image is too small to embed the data.");
     }
 
-    private static string ExtractAllBits(Bitmap image)
+    public static byte[] ExtractBytes(Bitmap image)
     {
-        var sb = new StringBuilder();
-        for (int x = 0; x < image.Width; x++)
+        string first36Bits = ExtractBitsFromPixels(image, 0, 6);
+        if (first36Bits.Length < 32)
+            return Array.Empty<byte>();
+
+        byte[] lengthBytes = new byte[4];
+        for (int i = 0; i < 4; i++)
+            lengthBytes[i] = Convert.ToByte(first36Bits.Substring(i * 8, 8), 2);
+
+        if (BitConverter.IsLittleEndian)
+            Array.Reverse(lengthBytes);
+
+        int dataLength = BitConverter.ToInt32(lengthBytes, 0);
+
+        if (dataLength <= 0 || dataLength > 100 * 1024 * 1024)
+            return Array.Empty<byte>();
+
+        int totalBitsNeeded = 32 + dataLength * 8;
+        int pixelsNeeded = (int)Math.Ceiling(totalBitsNeeded / 6.0);
+
+        string allBits = ExtractBitsFromPixels(image, 0, pixelsNeeded);
+        if (allBits.Length < totalBitsNeeded)
+            return Array.Empty<byte>();
+
+        byte[] result = new byte[dataLength];
+        for (int i = 0; i < dataLength; i++)
         {
-            for (int y = 0; y < image.Height; y++)
+            result[i] = Convert.ToByte(allBits.Substring(32 + i * 8, 8), 2);
+        }
+
+        return result;
+    }
+
+    private static string ExtractBitsFromPixels(Bitmap image, int startPixel, int pixelsCount)
+    {
+        if (pixelsCount <= 0)
+            return string.Empty;
+
+        int startX = startPixel / image.Height;
+        int startY = startPixel % image.Height;
+
+        var sb = new StringBuilder(pixelsCount * 6);
+        int pixelsRead = 0;
+
+        for (int x = startX; x < image.Width && pixelsRead < pixelsCount; x++)
+        {
+            int yStart = (x == startX) ? startY : 0;
+            for (int y = yStart; y < image.Height && pixelsRead < pixelsCount; y++)
             {
                 Color pixel = image.GetPixel(x, y);
-                var (rBits, gBits, bBits) = ToBinary(pixel);
-                sb.Append(ReadFirstBits(rBits, 2));
-                sb.Append(ReadFirstBits(gBits, 2));
-                sb.Append(ReadFirstBits(bBits, 2));
+
+                int rBits = pixel.R & 0x03;
+                int gBits = pixel.G & 0x03;
+                int bBits = pixel.B & 0x03;
+
+                sb.Append(Convert.ToString(rBits, 2).PadLeft(2, '0'));
+                sb.Append(Convert.ToString(gBits, 2).PadLeft(2, '0'));
+                sb.Append(Convert.ToString(bBits, 2).PadLeft(2, '0'));
+
+                pixelsRead++;
             }
         }
+
         return sb.ToString();
     }
 }
